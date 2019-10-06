fab5ec540bf913c8d9789cfd42aa2a00148f07d1
XIAO100he <xiao100he@gmil.com>
2019-10-06 17:11:45 +0900

    Make boy stories

---
+++ boyStory.php
@@ -0,0 +1,21 @@
+<?php
+require('head.php');
+?>
+<body>
+	<div class="all-wrapper">
+		<h1>SAVE THE TEETH</h1>
+		<p class="story-wrapper">
+			たろうくんは  &thinsp; はみきがきらいです<br>
+			おかあさんに「はみがきをちゃんとしてね」と<br>
+			いわれているのに、ちゃんと &thinsp;はをみがきません
+		</p>
+		<p class="story-next">
+			<a href="boyStory2.php">▶︎つぎへ</a>
+		</p>
+		<div class="bother-face">
+			<img src="img/kids-kenta005.png" alt="">
+		</div>
+	</div>
+	<script src=""></script>
+</body>
+</html>
---
+++ boyStory2.php
@@ -0,0 +1,21 @@
+<?php
+require('head.php');
+?>
+<body>
+	<div class="all-wrapper">
+		<h1>SAVE THE TEETH</h1>
+		<p class="story-wrapper">
+			あるひ、たろうくんは「はがいたい」といって<br>
+			ないていました<br>
+			みんなで &thinsp; たろうくんのくちのなかをみてみましょう
+		</p>
+		<p class="story-next">
+			<a href="selectTeeth.php" class="next-btn">▶︎つぎへ</a>
+		</p>
+		<div class="bother-face">
+			<img src="img/kids-kenta008.png" alt="">
+		</div>
+	</div>
+	<script src=""></script>
+</body>
+</html>
