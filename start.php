75521d2fc74983671399ab47d03faf5f64a7fa18
XIAO100he <xiao100he@gmil.com>
2019-10-06 17:11:29 +0900

    Make start.php

---
+++ start.php
@@ -0,0 +1,20 @@
+<?php
+require('head.php');
+?>
+<body>
+	<div class="all-wrapper">
+		<h1>SAVE THE TEETH</h1>
+		<p class="start-explain">
+			くちのなかのばいきんと &thinsp; たたかうゲームです<br>
+			きみは &thinsp; いくつ&thinsp; ばいきんをたおせるかな？
+		</p>
+		<div class="start-btn">
+			<a href="select.php">
+				<img src="img/character_kanban.png" alt="" class="start-img">
+				<p class="start-game">▶︎ゲームをはじめる</p>
+			</a>
+		</div>
+	</div>
+	<script src=""></script>
+</body>
+</html>
