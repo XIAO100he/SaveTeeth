4feeee6d06b69f7d619f1cde0ae8fe24cf65e3fe
XIAO100he <xiao100he@gmil.com>
2019-10-06 17:12:02 +0900

    Make selectTeeth.php

---
+++ selectTeeth.php
@@ -0,0 +1,26 @@
+<?php
+require('head.php');
+?>
+<body>
+	<div class="all-wrapper">
+		<h1>SAVE THE TEETH</h1>
+		<div class="bother-teeth">
+			<img src="img/character_sad.png" alt="" class="teeth-left">
+			<img src="img/character_dirt.png" alt="" class="teeth-right">
+		</div>
+		<p class="teeth-sentence">
+			くちのなかのばいきんが&thinsp;はをいじめているみたい<br>
+			このままにしておくと、もっといたくなってしまうよ
+		</p>
+		<div class="select-btns">
+			<div class="backFirst">
+				<a href="select.php" class="backFirst-btn">▶︎さいしょにもどる</a>
+			</div>
+			<div class="matchStart">
+				<a href="index.php" class="matchStart-btn">▶︎ばいきんとたたかう</a>
+			</div>
+		</div>
+	</div>
+	<script src=""></script>
+</body>
+</html>
