6022180e041d82e724f2ded799325297deb13cf6
XIAO100he <xiao100he@gmil.com>
2019-10-06 17:11:52 +0900

    Make girl stories

---
+++ girlStory2.php
@@ -0,0 +1,21 @@
+<?php
+require('head.php');
+?>
+<body>
+	<div class="all-wrapper">
+		<h1>SAVE THE TEETH</h1>
+		<p class="story-wrapper">
+			あるひ、あやちゃんは「はがいたい」といって<br>
+			ないていました<br>
+			みんなで &thinsp; あやちゃんのくちのなかをみてみましょう
+		</p>
+		<p class="story-next">
+			<a href="selectTeeth.php" class="next-btn">▶︎つぎへ</a>
+		</p>
+		<div class="bother-face">
+			<img src="img/kids-yukihiko008.png" alt="">
+		</div>
+	</div>
+	<script src=""></script>
+</body>
+</html>
