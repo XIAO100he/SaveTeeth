69ae77633573f513e597b4ad897f1132d58e63f6
XIAO100he <xiao100he@gmil.com>
2019-10-06 17:11:17 +0900

    Make select.php

---
+++ select.php
@@ -0,0 +1,25 @@
+<?php
+require('head.php');
+?>
+<body>
+	<div class="all-wrapper">
+		<h1>SAVE THE TEETH</h1>
+		<p class="select-sentence">
+			えらんでください
+		</p>
+		<div class="gender-wrapper">
+			<a href="boyStory.php">
+				<img src="img/kids-kenta001.png" alt="">
+				<p>おとこのこ</p>
+			</a>
+		</div>
+		<div class="gender-wrapper">
+			<a href="girlStory.php">
+				<img src="img/kids-yukihiko001.png" alt="">
+				<p>おんなのこ</p>
+			</a>
+		</div>
+	</div>
+	<script src=""></script>
+</body>
+</html>
