1dea8f4d9fd55064162a72b744021e34f362de5a
XIAO100he <xiao100he@gmil.com>
2019-10-08 20:29:34 +0900

    Add gameOver.php

---
+++ gameOver.php
@@ -0,0 +1,19 @@
+<?php
+require('head.php');
+?>
+<body>
+	<div class="all-wrapper">
+		<h1>SAVE THE TEETH</h1>
+      <p class="start-explain">
+        おしまい<br>
+        みんなのおかげで&thinsp;きれいな&thinsp;は&thinsp;になったよ！
+      </p>
+      <div class="start-btn">
+          <img src="img/character_coating.png" alt="" class="start-img">
+      </div>
+      <div class="play-again">
+				<a href="index.php" class="playAgain-btn">▶︎もういちどあそぶ</a>
+			</div>
+	</div>
+</body>
+</html>
