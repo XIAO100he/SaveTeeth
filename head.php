44833d61393f81e08ad35b3763bdbb18780a74cc
XIAO100he <xiao100he@gmil.com>
2019-10-08 18:44:15 +0900

    Add jquery

--- head.php
+++ head.php
@@ -6,4 +6,6 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 		<link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
+		<link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
+		<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 	</head>
